f1da62aec78f03b76e39b096c0aef49f75db0395
Sajal Singhal <ss.13052001@outlook.com>
2022-01-11 19:49:59 +0530

    added tabs to browse other sections

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from '@material-ui/icons/Email';
+import { useState } from "react";
+
 function App() {
+  const [tab, setTab] = useState(0);
   return (
     <Content>
       <SocialInfo>
         <Section>
           <ProfilePic src = {pic} />
           <h1>SAJAL SINGHAL</h1> 
+          {(tab == 0) && 
+          <div style = {{display: "flex", fontSize:"1.5rem"}}>
+            <TabChangeButton onClick = {() => {setTab(1)}}>Projects</TabChangeButton> |
+            <TabChangeButton onClick = {() => {setTab(2)}}>Skills</TabChangeButton> |
+            <TabChangeButton onClick = {() => {setTab(3)}}>Experience</TabChangeButton>
+          </div>
+          }
+          {(tab != 0) && <TabChangeButton onClick = {() => {setTab(0)}}>Hide Tab</TabChangeButton>}
         </Section>
         <SeperatorLine />
         <Section>
@@ -19,9 +30,9 @@ function App() {
             <a href="mailto:sajal.singhal1@gmail.com"><EmailIcon style = {{fontSize: "3.5rem"}}/></a>
         </Section>
       </SocialInfo>
-      <Projects />
-      <Skills />
-      <Experience />
+      {(tab == 1)&&<Projects />}
+      {(tab == 2)&&<Skills />}
+      {(tab == 3)&&<Experience />}
     </Content>
   );
 }
@@ -32,7 +43,8 @@ const Content = styled.div`
   height: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
 const Section = styled.div`
@@ -61,13 +73,35 @@ const SocialInfo = styled.div`
   justify-content: center;
 `;
 
+const TabChangeButton = styled.button`
+  margin: 5px;
+  border: none;
+  background-color: inherit;
+  color: inherit;
+  font-size: 0.9rem;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
 const ProfilePic = styled.img`
   height: 25vh;
   width: 25vh;
   border-radius: 50%;
 `;
 
-const Projects = styled.div``;
-const Skills = styled.div``;
-const Experience = styled.div``;
+const Projects = styled.div`
+  background-color: white;
+  width: 100px;
+  height: 100px;
+`;
+const Skills = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 100px;
+`;
+const Experience = styled.div`
+  background-color: blue;
+  width: 100px;
+  height: 100px;
+`;
 export default App;
